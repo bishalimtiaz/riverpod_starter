6151b87e9e8534b4241cec359804d32ec0600878
Momshad Dinury <dinurymomshad.dev@gmail.com>
2022-08-16 15:43:48 +0600

    Documentation comment should start with '///'.

--- lib/flavors/env_config.dart
+++ lib/flavors/env_config.dart
@@ -16,18 +16,24 @@ class EnvConfig {
   }) {
     logger = Logger(
       printer: PrettyPrinter(
-          methodCount: AppValues.loggerMethodCount,
-          // number of method calls to be displayed
-          errorMethodCount: AppValues.loggerErrorMethodCount,
-          // number of method calls if stacktrace is provided
-          lineLength: AppValues.loggerLineLength,
-          // width of the output
-          colors: true,
-          // Colorful log messages
-          printEmojis: true,
-          // Print an emoji for each log message
-          printTime: false // Should each log print contain a timestamp
-          ),
+        /// Number of method calls to be displayed
+        methodCount: AppValues.loggerMethodCount,
+
+        /// Number of method calls if stacktrace is provided
+        errorMethodCount: AppValues.loggerErrorMethodCount,
+
+        /// Width of the output
+        lineLength: AppValues.loggerLineLength,
+
+        /// Colorful log messages
+        colors: true,
+
+        /// Print an emoji for each log message
+        printEmojis: true,
+
+        /// Should each log print contain a timestamp
+        printTime: false,
+      ),
     );
   }
 }
